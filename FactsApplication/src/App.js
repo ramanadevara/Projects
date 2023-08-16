d3a017e840380e9742e46905e42970ef2df73ac6
unknown <venkatdevara98@gmail.com>
2023-08-16 09:36:25 +0530

    React: divides interface into components

--- FactsApplication/src/App.js
+++ FactsApplication/src/App.js
@@ -2,15 +2,33 @@ import "./style.css"
 
 function App() {
   return (
-    <header className='header'>
-      <div className='logo'>
-        <img src='logo.png' alt='message logo' />
+    <>
+      <header className='header'>
+        <div className='logo'>
+          <img src='logo.png' alt='message logo' />
 
-        <h1>Today I learned</h1>
-      </div>
-      <button className='btn btn-large btn-fact'>Share a fact</button>
-    </header>
+          <h1>Today I learned</h1>
+        </div>
+        <button className='btn btn-large btn-fact'>Share a fact</button>
+      </header>
+      <FactsForm />
+      <main className='main'>
+        <Categories />
+        <FactsList />
+      </main>
+    </>
   )
 }
 
+function FactsForm() {
+  return <form className='fact-form'>FactsForm</form>
+}
+
+function Categories() {
+  return <aside> Categories</aside>
+}
+
+function FactsList() {
+  return <section>Facts List</section>
+}
 export default App
